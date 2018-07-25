81ebfe1c78af783fd789a3288d2e970c1ca26798
Benjamin Rachi <benjaminrachi@gmail.com>
2018-07-26 00:32:31 +0300

    add login page in client

--- asos-client/src/app/app.module.ts
+++ asos-client/src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule }    from '@angular/forms';
+
+import {AlertService} from './services/alerts/alerts.service';
+import {AuthGuard} from './services/_guard/auth.guard';
+import {AuthenticationService} from './services/authentication/authentication.service';
+import {CartService} from './services/cart/cart.service';
+import {ProductsService} from './services/products/products.service';
+import {UserService} from './services/user/user.service';
+
 
 import { AppComponent } from './app.component';
 import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatListModule, MatBottomSheetModule } from '@angular/material';
-import { NavigationComponent } from './navigation/navigation.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
@@ -15,6 +24,8 @@ import { RecommendedProductsComponent } from './components/recommended-products/
 import { DepartmentComponent } from './components/department/department.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ShareProductComponent } from './components/share-product/share-product.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AlertComponent } from './components/alert/alert.component';
 
 @NgModule({
   declarations: [
@@ -26,12 +37,15 @@ import { ShareProductComponent } from './components/share-product/share-product.
     RecommendedProductsComponent,
     DepartmentComponent,
     ProductDetailsComponent,
-    ShareProductComponent
+    ShareProductComponent,
+    RegisterComponent,
+    AlertComponent
   ],
   imports: [
     HttpClientModule,
     AppRoutingModule,
     BrowserModule,
+    ReactiveFormsModule,  
     MatGridListModule,
     MatCardModule,
     MatMenuModule,
@@ -45,6 +59,13 @@ import { ShareProductComponent } from './components/share-product/share-product.
     BrowserAnimationsModule
   ],
   providers: [
+    AlertService,
+    AuthGuard,
+    AuthenticationService,
+    CartService,
+    ProductsService,
+    UserService
+
   ],
   entryComponents: [AppComponent, ShareProductComponent],
   bootstrap: [AppComponent]
--- asos-server/api/users.js
+++ asos-server/api/users.js
@@ -1,6 +1,10 @@
 
 const { Router } = require("express");
-const {authenticatedUser } = require("../middleware/authenticated");
+const { check } = require('express-validator/check');
+
+const { authenticatedUser } = require("../middleware/authenticated");
+const { rejectInvalidRequest } = require("../middleware/rejectInvalidRequests");
+
 const { UsersController } = require("../controllers/users.controller");
 
 const router = Router();
@@ -8,5 +12,12 @@ const router = Router();
 router.get("/me", authenticatedUser(), UsersController.getCurrentUser);
 
 router.post("/logout", UsersController.logout);
+
+const validateLogin = [
+    check('username').isLength({ min: 5, max: 30 }).isAlphanumeric().exists(),
+    check('password').isLength({ min: 5, max: 30 }).isAscii().exists()
+];
+
+router.post("/login", validateLogin, rejectInvalidRequest, UsersController.login);
 
 module.exports = router;
