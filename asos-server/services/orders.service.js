3619f46922cb8459588f3e38b4129de1f87a8c22
shai atias <atias.shai@gmail.com>
2018-08-27 21:56:32 +0300

    missing file

---
+++ asos-server/services/orders.service.js
@@ -0,0 +1,37 @@
+
+const {Order} = require("../db/orders");
+
+const sleep = (time) => new Promise(resolve => setTimeout(resolve, time));
+
+class OrdersService {
+
+    static async chargeClient(name, cardNum, mm, yyyy, cvv, amount) {
+
+        await sleep(1000);
+
+        return {
+            amount,
+            chargeDate: new Date(),
+            transactionId: Math.floor(Math.random() * 100000)
+        }
+    }
+
+    static async createOrder(user, cart, payment) {
+
+        const {name, cardNumber, expireMonth, expireYear, cvv} = payment;
+        const {totalPrice: amount} = cart;
+
+        const chargeResult = this.chargeClient(name, cardNumber, expireMonth, expireYear, cvv, amount);
+
+        return await Order.create({
+            user: user._id,
+            products: cart.products,
+            createdDate: new Date(),
+            chargeResult
+        });
+
+    }
+
+}
+
+module.exports = {OrdersService};
